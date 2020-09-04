8b6b02f21648674c47d02df22a0c5673a4b83488
mwerensteijn <mitchellwerensteijn@msn.com>
2020-09-04 17:24:27 +0200

    64-bit build & clean scripts

---
+++ clean64.sh
@@ -0,0 +1,6 @@
+    ARCH=aarch64 \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CFLAGS="-Wno-macro-redefined `pkg-config --cflags libprotobuf-c` -D ANDROID_PLATFORM=android-27 -D CONFIG_HAS_TCP_REPAIR_WINDOW -D CONFIG_HAS_TCP_REPAIR -D CONFIG_HAS_FSCONFIG -D NO_SE_LINUX -D CONFIG_HAS_PTRACE_PEEKSIGINFO" \
+    LDFLAGS="`pkg-config --libs libprotobuf-c` `pkg-config --libs libnl-3.0` `pkg-config --libs libnet`" \
+    make clean
+
